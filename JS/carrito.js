8e32df91764e7cb464c9b317e8c826e968cdf524
Eliana Rojas <eriroj@yahoo.com.ar>
2024-04-11 19:48:15 -0300

    Hice modificaciones en js carrito, main, css, html e images

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,15 +1,42 @@
-let cartStorage = localStorage.getItem("cartProducts")
-cartStorage = JSON.parse(cartStorage)
+Carrito.addEventListener("click",()=> {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class= "modal-header-title">Carrito.</h1>`
+    modalContainer.append(modalHeader);
+    
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+    
+    modalbutton.addEventListener("click", () =>{
+        modalContainer.style.display = "none";
 
-let cartContainer = document.getElementById("cart-section")
+    });
+   
+    modalHeader.append(modalbutton);
 
-function renderCarrito (cartItems) {
-    cartItems.forEach (producto => {
-        const cart = document.createElement("div")
-        cart.innerHTML = `<h3>${producto.nombre}</h3>
-                          <p>${producto.precio}</p>`
-        cartContainer.appendChild(cart)
-    })
-}
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+        <p>cantidad: ${product.cantidad}</p>
+        <p>Total: ${product.cantidad * product.precio}</p>
+        `;
+        modalContainer.append(carritoContent);
+   });
+   const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad,0);
+    
+ 
+   const totalBuying = document.createElement("div");
+   totalBuying.className = "total-content";
+   totalBuying.innerHTML = `total a pagar: ${total}$`;
+   modalContainer.append(totalBuying);
 
-renderCarrito(cartStorage)
+   localStorage.setItem("carrito", JSON.stringify(carrito))
+});
--- JS/js main.js
+++ JS/js main.js
@@ -11,7 +11,7 @@ const productos = [
         img:"images/silla1.jpg",
         nombre: "Closer",
         precio: 2000,
-        
+        cantidad: 1,
     },
     
     {
@@ -19,6 +19,7 @@ const productos = [
         img:"images/silla2.jpg",
         nombre: "Angel",
         precio: 1500,
+        cantidad: 1,
     },
     
     {
@@ -26,12 +27,14 @@ const productos = [
         img:"images/silla3.jpg",
         nombre: "Lookhere",
         precio: 2000,
+        cantidad: 1,
     },
     {
         id: 4,
         img:"images/silla4.jpg",
         nombre: "So far away",
         precio: 1800,
+        cantidad: 1,
     },
     
     {
@@ -39,12 +42,14 @@ const productos = [
         img:"images/silla5.jpg",
         nombre: "Singularity",
         precio: 3000,
+        cantidad: 1,
     },
     {
         id: 6,
         img:"images/silla6.jpg",
         nombre: "EveryThingoes",
         precio: 2800,
+        cantidad: 1,
     },
     
     {
@@ -52,6 +57,7 @@ const productos = [
         img:"images/silla7.jpg",
         nombre: "Film out",
         precio: 2500,
+        cantidad: 1,
     },
     
     
@@ -60,12 +66,14 @@ const productos = [
         img:"images/silla8.jpg",
         nombre: "Little tulip",
         precio: 3500,
+        cantidad: 1,
     },
     {
         id: 9,
         img:"images/silla9.jpg",
         nombre: "Artek",
         precio: 3000,
+        cantidad: 1,
     },
     
     {
@@ -73,18 +81,21 @@ const productos = [
         img:"images/silla10.jpg",
         nombre: "Butterfly",
         precio: 3200,
+        cantidad: 1,
     },
     {
         id: 11,
         img:"images/silla11.jpg",
         nombre: "Jacobsen",
         precio: 3000,
+        cantidad: 1,
     },
     {
         id: 12,
         img:"images/silla12.jpg",
         nombre: "Blue side",
         precio: 2800,
+        cantidad: 1,
     },
     
     {
@@ -92,12 +103,14 @@ const productos = [
         img:"images/silla13.jpg",
         nombre: "Lie",
         precio: 2500,
+        cantidad: 1,
     },
     {
         id: 14,
         img:"images/silla14.jpg",
         nombre: "Jamais Vu",
         precio: 3000,
+        cantidad: 1,
     },
     
     {
@@ -105,48 +118,56 @@ const productos = [
         img:"images/silla15.jpg",
         nombre: "Eames",
         precio: 3800,
+        cantidad: 1,
     },
     {
         id: 16,
         img:"images/silla16.jpg",
         nombre: "Índigo",
         precio: 2800,
+        cantidad: 1,
     },
     {
         id: 17,
         img:"images/silla17.jpg",
         nombre: "Cook Loom",
         precio: 2500,
+        cantidad: 1,
     },
     {
         id: 18,
         img:"images/silla18.jpg",
         nombre: "Elephant",
         precio: 2700,
+        cantidad: 1,
     },
     {
         id: 19,
         img:"images/silla19.jpg",
         nombre: "Home",
         precio: 3000,
+        cantidad: 1,
     },
     {
         id: 20,
         img:"images/silla20.jpg",
         nombre: "So far away",
         precio: 2800,
+        cantidad: 1,
     },
     {
         id: 21,
         img:"images/silla21.jpg",
         nombre: "Snooze",
         precio: 2500,
+        cantidad: 1,
     },
     {
         id: 22,
         img:"images/silla22.jpg",
         nombre: "Bo Concept",
         precio: 3000,
+        cantidad: 1,
     },
     
     {
@@ -154,21 +175,23 @@ const productos = [
         img:"images/silla23.jpg",
         nombre: "Epiphany",
         precio: 3500,
+        cantidad: 1,
     },
     {
         id: 24,
         img:"images/silla24.jpg",
         nombre: "My Time",
         precio: 3000,
+        cantidad: 1,
     },
     ]
     let carrito = [];
    
-    let cartProductsLS = localStorage.getItem("cartProducts")
-if(cartProductsLS){
-    cartProducts = JSON.parse(cartProductsLS)
+    let carritoLS = localStorage.getItem("carrito")
+if(carritoLS){
+    carito = JSON.parse(carritoLS)
 } else {
-    cartProducts = []
+    carrito = []
 }
 
     productos.forEach((product)=>{
@@ -178,6 +201,7 @@ if(cartProductsLS){
         <img src="${product.img}">
         <h3>${product.nombre}</h3>
         <p>${product.precio} $</p>
+        <p>cantidad: ${product.cantidad}</p>
         `
         
             
@@ -188,53 +212,24 @@ if(cartProductsLS){
             content.append(comprar);
 
             comprar.addEventListener("click", ()=>{
+                const repeat = carrito.some((repeatProduct) => repeatProduct.id===product.id);
+                if (repeat) {
+                    carrito.map((prod) => {
+                        if (prod.id === product.id) {
+                            prod.cantidad++;
+                        }
+                    })
+                } else {
+
              carrito.push({
                 id: product.id,
                 img: product.img,
                 nombre: product.nombre,
                 precio: product.precio,
+                cantidad: product.cantidad,
 
             });
-        
+                }
     });
 });
-    Carrito.addEventListener("click",()=> {
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "flex";
-    const modalHeader = document.createElement("div");
-    modalHeader.className = "modal-header";
-    modalHeader.innerHTML = `
-    <h1 class= "modal-header-title">Carrito.</h1>`
-    modalContainer.append(modalHeader);
     
-    const modalbutton = document.createElement("h1");
-    modalbutton.innerText = "x";
-    modalbutton.className = "modal-header-button";
-    
-    modalbutton.addEventListener("click", () =>{
-        modalContainer.style.display = "none";
-
-    });
-   
-    modalHeader.append(modalbutton);
-
-    carrito.forEach((product) => {
-        let carritoContent = document.createElement("div");
-        carritoContent.className = "modal-content";
-        carritoContent.innerHTML = `
-        <img src="${product.img}">
-        <h3>${product.nombre}</h3>
-        <p>${product.precio} $</p>
-        `;
-        modalContainer.append(carritoContent);
-   });
-   const total = carrito.reduce((acc, el) => acc + el.precio,0);
-    
- 
-   const totalBuying = document.createElement("div");
-   totalBuying.className = "total-content";
-   totalBuying.innerHTML = `total a pagar: ${total}$`;
-   modalContainer.append(totalBuying);
-
-   localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
-});
