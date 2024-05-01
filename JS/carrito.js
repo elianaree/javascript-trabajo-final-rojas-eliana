41e12354c7813f717898b0f7768847e6fc517f69
Eliana Rojas <eriroj@yahoo.com.ar>
2024-05-01 19:49:02 -0300

    Borré y modifiqué el index, tambíen creé nuevos archivos

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,10 +1,13 @@
-Carrito.addEventListener("click",()=> {
+
+const pintarCarrito = () =>{
     modalContainer.innerHTML = "";
     modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
-    <h1 class= "modal-header-title">Carrito.</h1>`
+    <button class= "btn">Comprar</button>
+    <h1 class= "modal-header-title">Carrito</h1>
+    `
     modalContainer.append(modalHeader);
     
     const modalbutton = document.createElement("h1");
@@ -18,25 +21,89 @@ Carrito.addEventListener("click",()=> {
    
     modalHeader.append(modalbutton);
 
-    carrito.forEach((product) => {
+   document.getElementsByClassName('btn')[0].addEventListener('click', comprarClicked)
+function comprarClicked() {
+    Swal.fire("¡Gracias por su compra!");
+};
+   
+       carrito.forEach((product) => {
         let carritoContent = document.createElement("div");
         carritoContent.className = "modal-content";
         carritoContent.innerHTML = `
         <img src="${product.img}">
         <h3>${product.nombre}</h3>
-        <p>${product.precio} $</p>
+        <p> $ ${product.precio}</p>
+        <span class="restar"> - </span>
         <p>cantidad: ${product.cantidad}</p>
+        <span class="sumar"> + </span>
         <p>Total: ${product.cantidad * product.precio}</p>
-        `;
-        modalContainer.append(carritoContent);
-   });
-   const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad,0);
     
- 
+
+         `;
+        modalContainer.append(carritoContent);
+
+        let restar = carritoContent.querySelector(".restar")
+        restar.addEventListener("click",() => {
+            if (product.cantidad !== 1){
+            product.cantidad--;
+            }
+            
+            pintarCarrito();
+
+        });
+        let sumar = carritoContent.querySelector(".sumar")
+        sumar.addEventListener("click",() => {
+            
+            product.cantidad++;
+            pintarCarrito();
+
+        });
+
+
+
+        let eliminar = document.createElement("span");
+        eliminar.innerText = "X";
+        eliminar.className = "delete-product";
+        carritoContent.append(eliminar);
+        eliminar.addEventListener("click", eliminarProducto);
+
+        });
+   
+       
+   
+   const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad,0);
+  
    const totalBuying = document.createElement("div");
    totalBuying.className = "total-content";
-   totalBuying.innerHTML = `total a pagar: ${total}$`;
+   totalBuying.innerHTML = `Total a pagar: $ ${total}`;
    modalContainer.append(totalBuying);
 
-   localStorage.setItem("carrito", JSON.stringify(carrito))
-});
+
+ 
+localStorage.setItem("carrito", JSON.stringify(carrito))
+      };
+     
+     
+     Carrito.addEventListener("click", pintarCarrito);
+
+  const eliminarProducto = () => {
+    const foundId = carrito.find((element) => element.id);
+
+    carrito = carrito.filter((carritoId)=> {
+        return carritoId !== foundId;
+    });
+    carritoCounter();
+    pintarCarrito();
+    
+  };
+
+
+const carritoCounter = () => {
+cantidadCarrito.style.display = "block";
+cantidadCarrito.innerText = carrito.length;
+
+  
+
+
+};
+
--- JS/js main.js
+++ JS/js main.js
@@ -3,12 +3,13 @@
 const shopContent = document.getElementById("shopContent");
 const Carrito = document.getElementById("carrito");
 const modalContainer = document.getElementById("modal-container");
-
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+const vaciarCarrito = document.getElementById("vaciar-carrito");
 //Array de objetos
 const productos = [
     {
         id: 1,
-        img:"images/silla1.jpg",
+        img:"assets/images/silla1.jpg",
         nombre: "Closer",
         precio: 2000,
         cantidad: 1,
@@ -16,7 +17,7 @@ const productos = [
     
     {
         id: 2,
-        img:"images/silla2.jpg",
+        img:"assets/images/silla2.jpg",
         nombre: "Angel",
         precio: 1500,
         cantidad: 1,
@@ -24,14 +25,14 @@ const productos = [
     
     {
         id: 3,
-        img:"images/silla3.jpg",
+        img:"assets/images/silla3.jpg",
         nombre: "Lookhere",
         precio: 2000,
         cantidad: 1,
     },
     {
         id: 4,
-        img:"images/silla4.jpg",
+        img:"assets/images/silla4.jpg",
         nombre: "So far away",
         precio: 1800,
         cantidad: 1,
@@ -39,14 +40,14 @@ const productos = [
     
     {
         id: 5,
-        img:"images/silla5.jpg",
+        img:"assets/images/silla5.jpg",
         nombre: "Singularity",
         precio: 3000,
         cantidad: 1,
     },
     {
         id: 6,
-        img:"images/silla6.jpg",
+        img:"assets/images/silla6.jpg",
         nombre: "EveryThingoes",
         precio: 2800,
         cantidad: 1,
@@ -54,7 +55,7 @@ const productos = [
     
     {
         id: 7,
-        img:"images/silla7.jpg",
+        img:"assets/images/silla7.jpg",
         nombre: "Film out",
         precio: 2500,
         cantidad: 1,
@@ -63,14 +64,14 @@ const productos = [
     
     {
         id: 8,
-        img:"images/silla8.jpg",
+        img:"assets/images/silla8.jpg",
         nombre: "Little tulip",
         precio: 3500,
         cantidad: 1,
     },
     {
         id: 9,
-        img:"images/silla9.jpg",
+        img:"assets/images/silla9.jpg",
         nombre: "Artek",
         precio: 3000,
         cantidad: 1,
@@ -78,21 +79,21 @@ const productos = [
     
     {
         id: 10,
-        img:"images/silla10.jpg",
+        img:"assets/images/silla10.jpg",
         nombre: "Butterfly",
         precio: 3200,
         cantidad: 1,
     },
     {
         id: 11,
-        img:"images/silla11.jpg",
+        img:"assets/images/silla11.jpg",
         nombre: "Jacobsen",
         precio: 3000,
         cantidad: 1,
     },
     {
         id: 12,
-        img:"images/silla12.jpg",
+        img:"assets/images/silla12.jpg",
         nombre: "Blue side",
         precio: 2800,
         cantidad: 1,
@@ -100,14 +101,14 @@ const productos = [
     
     {
         id: 13,
-        img:"images/silla13.jpg",
+        img:"assets/images/silla13.jpg",
         nombre: "Lie",
         precio: 2500,
         cantidad: 1,
     },
     {
         id: 14,
-        img:"images/silla14.jpg",
+        img:"assets/images/silla14.jpg",
         nombre: "Jamais Vu",
         precio: 3000,
         cantidad: 1,
@@ -115,56 +116,56 @@ const productos = [
     
     {
         id: 15,
-        img:"images/silla15.jpg",
+        img:"assets/images/silla15.jpg",
         nombre: "Eames",
         precio: 3800,
         cantidad: 1,
     },
     {
         id: 16,
-        img:"images/silla16.jpg",
+        img:"assets/images/silla16.jpg",
         nombre: "Índigo",
         precio: 2800,
         cantidad: 1,
     },
     {
         id: 17,
-        img:"images/silla17.jpg",
+        img:"assets/images/silla17.jpg",
         nombre: "Cook Loom",
         precio: 2500,
         cantidad: 1,
     },
     {
         id: 18,
-        img:"images/silla18.jpg",
+        img:"assets/images/silla18.jpg",
         nombre: "Elephant",
         precio: 2700,
         cantidad: 1,
     },
     {
         id: 19,
-        img:"images/silla19.jpg",
+        img:"assets/images/silla19.jpg",
         nombre: "Home",
         precio: 3000,
         cantidad: 1,
     },
     {
         id: 20,
-        img:"images/silla20.jpg",
+        img:"assets/images/silla20.jpg",
         nombre: "So far away",
         precio: 2800,
         cantidad: 1,
     },
     {
         id: 21,
-        img:"images/silla21.jpg",
+        img:"assets/images/silla21.jpg",
         nombre: "Snooze",
         precio: 2500,
         cantidad: 1,
     },
     {
         id: 22,
-        img:"images/silla22.jpg",
+        img:"assets/images/silla22.jpg",
         nombre: "Bo Concept",
         precio: 3000,
         cantidad: 1,
@@ -172,27 +173,30 @@ const productos = [
     
     {
         id: 23,
-        img:"images/silla23.jpg",
+        img:"assets/images/silla23.jpg",
         nombre: "Epiphany",
         precio: 3500,
         cantidad: 1,
     },
     {
         id: 24,
-        img:"images/silla24.jpg",
+        img:"assets/images/silla24.jpg",
         nombre: "My Time",
         precio: 3000,
         cantidad: 1,
     },
     ]
+
     let carrito = [];
    
     let carritoLS = localStorage.getItem("carrito")
 if(carritoLS){
-    carito = JSON.parse(carritoLS)
+    carrito = JSON.parse(carritoLS)
 } else {
     carrito = []
 }
+
+
 
     productos.forEach((product)=>{
         let content = document.createElement("div");
@@ -228,8 +232,49 @@ if(carritoLS){
                 precio: product.precio,
                 cantidad: product.cantidad,
 
-            });
+                });
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Producto añadido",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
                 }
+          carritoCounter();      
     });
 });
+const getProducts = async () => {
+
+    const response = await fetch ("./db/data.json");
+    const data = await response.json();
+
+ data.forEach(product =>{
+        const productos = document.createElement("div")
+        card.innerHTML = `Img src = ${product.img}>
+                          <h3>Nombre: ${product.nombre}</h3>
+                          <p>Precio: $${product.precio}</p>
+                          <p>Cantidad: ${product.cantidad}</p>
+                          <button>Comprar</button>`
+                          
+        container.appenChild(productos)
+
+    });
+};
+
+
     
+ 
+
+    
+   
+    
+    
+    
+
+
+
+
+
+
+   
